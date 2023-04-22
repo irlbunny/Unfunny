83e08b7b2c01e2c2e01dc8e7e00f884d0ce3289c
kaitlyn~ <ItsKaitlyn03@protonmail.com>
2023-04-22 11:32:33 -0400

    Initial source

---
+++ Unfunny/HarmonyPatches/DateTime.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System;
+
+namespace Unfunny.HarmonyPatches
+{
+    [HarmonyPatch(typeof(DateTime), "get_Now")]
+    internal class DateTimeNow
+    {
+        private static void Postfix(ref DateTime __result)
+        {
+            var month = __result.Month;
+            var day = __result.Day;
+            if ((month == 4 && day == 1) || (month == 4 && day == 22))
+                __result = __result.AddDays(1); // Skip to April 2nd or April 23rd, thus skipping april fools and earth day.
+        }
+    }
+}
---
+++ Unfunny/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using IPA;
+using System.Reflection;
+using IPALogger = IPA.Logging.Logger;
+
+namespace Unfunny
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal const string HARMONYID = "com.github.ItsKaitlyn03.Unfunny";
+
+        internal static IPALogger Log { get; private set; }
+        internal static Harmony HarmonyInstance { get; private set; } = new(HARMONYID);
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Log = logger;
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        { }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            HarmonyInstance.UnpatchSelf();
+        }
+    }
+}
